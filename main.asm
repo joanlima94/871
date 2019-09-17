35ccb06fe1465a32fcab2aecb552b2713fc22643
joanlima94 <48864868+joanlima94@users.noreply.github.com>
2019-09-17 20:53:37 -0300

    Update main.asm

--- main.asm
+++ main.asm
@@ -15,14 +15,56 @@ jmp main
 .org 0x0034
 
 main:
-;setar o bit 3 de ddrb, pino13
-	ldi r16, 0x10
-	out DDRB,r16
-	
+	           
+	lds r16, 0x24 //configurado o pino 5 como saida (ddrb)
+	ori r16, 0x20
+	sts 0x24, r16
+
+
 loop:
-;manter o led sempre aceso, bit 5 de PORTB
-	sbi PORTB,5
-	rjmp loop
+	
+	lds r16, 0x24
+	andi r16, 0x7F
+	sts 0x24, r16
+	jmp atrasa05
+	lds r16,0x24
+	ori r16, 0x80
+	jmp atrasa05
+	
+	rjmp loop //volta pro inicio do loop
 
-
+atrasa05:  //atraso de 0,5s
+	ldi r18, 200
+	ldi r19, 200
+	ldi r20, 200 
+	ldi r23, 0
+   volta:
+	  dec r18 //decrementa r18, começa em 200
+	  cp r18, r23
+	  brne volta //enquanto R18 >0 decrementa r18
+	  dec r19 //decrementa r19, começa em 200
+	  cp r19, r23
+	  brne volta //enquanto r19>0 decrementa r19
+	  dec r20  //decrementa r20, começa em 200
+	  cp r20, r23
+	  brne volta //enquanto r19>0 vai para volta
+	  ret
+	  
+atrasa01: //atraso de 0,1s
+	ldi r18, 200
+	ldi r19, 200
+	ldi r20, 40
+	ldi r23, 0
+	retorna:
+	  dec r18 //decrementa r18, começa em 200
+	  cp r18, r23
+	  brne retorna //enquantor 18 > 0 decrementa r18
+	  dec r19 //decrementa r18, começa em 200
+	  cp r19, r23
+	  brne retorna //enquanto r19 >0, decrementa r19
+	  dec r20 //decrementa r20, começa em 40
+	  cp r20, r23
+	  brne retorna //enquanto r20>0 vai para volta
+	  ret
+		   
 
