c6c8a0e0fb9f98fe7eb8b8b54dec1af6db528e40
joanlima94 <48864868+joanlima94@users.noreply.github.com>
2019-10-07 23:46:05 -0300

    Update atividade6.c

--- atividade6.c
+++ atividade6.c
@@ -18,7 +18,7 @@ unsigned char *ucsr0b;
 unsigned char *ucsr0c;
 //Registrador de dados
 unsigned char *udr0; 
-
+volatile int i = 0;
 
 void setar_registradores(void)
 {
@@ -30,20 +30,20 @@ void setar_registradores(void)
 	udr0 = (unsigned char *) 0xC6;
 	
 	//esses dois registradores setam o Baud-Rate em 38400 bps.
-	*ubrr0h &= 0;
-	*ubrr0l &= 25;
+	*ubrr0h = 0;
+	*ubrr0l = 25;
 	
 	/*Esse registrador indica as seguintes especificações:
 	-Velocidade de transmissão normal
 	-Multiprocessador desabilitado
 	*/
-	*ucsr0a &= 0x00;
+	*ucsr0a = 0b00000000;
 	/*Esse registrador indica as seguintes especificações:
 	-Todas as interrupções USART desabilitadas
 	-Somente o transmissor ativado
 	-Número de bits transmitidos em cada frame (UCSZ02), no caso são 8
 	*/
-	*ucsr0b &=0b00001000;  
+	*ucsr0b =0b00001000;  
 	/*Esse registrador indica as seguintes especificações:
 	-USART assincrono
 	-Bit de paridade ímpar
@@ -51,23 +51,34 @@ void setar_registradores(void)
 	-Número de bits transmitidos em cada frame (UCSZ01 e UCSZ00), no caso são 8
 	-Transmissão assíncrona
 	*/
-	*ucsr0c &=0b00110110;
+	*ucsr0c =0b00110110;
 }
 
 int main(void)
 {
 	setar_registradores();
-    char msg[] ="o bagulho nao ta funcionando"; 
-	int i=0;
-	
-	while (msg[i]!='\0') //condição de execução do loop, só para quando chega no último caracter da string \0
-    	{
-		*udr0=msg[i];
-		while((*ucsr0a & 0b01000000) != 0b01000000){}; //Sai do looping toda vez a flag de envio ativar
-		i++;
-		_delay_ms(1);
-    	}
-	i=0;	
-	
+        char msg[]="Estamos no processo de fazer o bagulho funcionar\n";
+    	char fim[]="A mensagem foi transmitida com sucesso!\n"; 
+
+	while(1)
+	{
+		i = 0;
+		while (msg[i]!='\0') //condição de execução do loop, só para quando chega no último caracter da string \0
+    		{
+			*udr0=msg[i];
+			while(!(*ucsr0a & 0b00100000)){}; //Sai do looping toda vez a flag de envio ativar
+    			i++;
+			_delay_ms(100);
+		}
+		i=0;
+		while(fim[i]!='\0')
+		{
+			*udr0=fim[i];
+			while(!(*ucsr0a & 0b00100000)){};
+			i++;
+			_delay_ms(100);
+		}
+		_delay_ms(5000);	
+	}
 }
 
