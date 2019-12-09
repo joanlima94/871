72c6855daa983978d3d95db62855a51caea1d13a
joanlima94 <48864868+joanlima94@users.noreply.github.com>
2019-12-08 21:54:50 -0300

    Rename main.c to atividade8.c

--- atividade8.c
+++ atividade8.c
@@ -19,22 +19,22 @@ void configura_uart()
 	UBRR0H = 0;
 	UBRR0L = 25;
 	
-	//Velocidade de transimissão normal e Multiprocessador desabilitado
+	//Velocidade de transimissÃ£o normal e Multiprocessador desabilitado
 	UCSR0A = 0x00;
 	
 	/*
-	- Transmissões USART desabilitadas
+	- TransmissÃµes USART desabilitadas
 	- Somente o transmissor ativo
 	- Numero de bits transmitidos em cada frame igual a 8
 	*/
 	UCSR0B = 0x08;
 	
-	/*Esse registrador indica as seguintes especificações:
+	/*Esse registrador indica as seguintes especificaÃ§Ãµes:
 	-USART assincrono
-	-Bit de paridade ímpar
-	-um único bit de parada
-	-Número de bits transmitidos em cada frame (UCSZ01 e UCSZ00), no caso são 8
-	-Transmissão assíncrona
+	-Bit de paridade Ã­mpar
+	-um Ãºnico bit de parada
+	-NÃºmero de bits transmitidos em cada frame (UCSZ01 e UCSZ00), no caso sÃ£o 8
+	-TransmissÃ£o assÃ­ncrona
 	*/
 	UCSR0C =0b00110110;
 	
@@ -42,8 +42,8 @@ void configura_uart()
 
 void configura_ledin()
 {
-	TCCR2A = 0x02; // Seleciona o modo de operação CTC
-	TCCR2B = 0xC2; //configura o prescaler para ter um fator 8, ou seja a frequência passa a ser 2MHz
+	TCCR2A = 0x02; // Seleciona o modo de operaÃ§Ã£o CTC
+	TCCR2B = 0xC2; //configura o prescaler para ter um fator 8, ou seja a frequÃªncia passa a ser 2MHz
 	OCR2A = 200; //Valor colocado no registrador que define a faixa de contagem 
 	OCR2B = 200; //Valor para ser comparado no item B
 	TIMSK2 = 0x06;
@@ -52,11 +52,11 @@ void configura_ledin()
 
 ISR(TIMER2_COMPA_vect)
 {
-	cont1++;  //Contador q deve contar até 5000 para bater 0.5s e acender o led
+	cont1++;  //Contador q deve contar atÃ© 5000 para bater 0.5s e acender o led
 	if (cont1==5000)
 	{
 		PORTB = 0x20;
-	} else if (cont1==10000)  // Essa condição é para manter o led 0.5 apagado
+	} else if (cont1==10000)  // Essa condiÃ§Ã£o Ã© para manter o led 0.5 apagado
 	{
 		PORTB&=0b11011111;
 		cont1=0;
@@ -66,11 +66,11 @@ ISR(TIMER2_COMPA_vect)
 
 ISR(TIMER2_COMPB_vect)
 {
-		cont2++;  //Contador q deve contar até 5000 para bater 0s 0.78s e acender o led
+		cont2++;  //Contador q deve contar atÃ© 5000 para bater 0s 0.78s e acender o led
 		if (cont2==7800)
 		{
 			PORTB = 0x10;
-		} else if (cont2==15600) // essa condição é para mantar o led 0.78s apagado
+		} else if (cont2==15600) // essa condiÃ§Ã£o Ã© para mantar o led 0.78s apagado
 		{
 			PORTB&=0b11101111;
 			cont2=0;
@@ -84,7 +84,7 @@ int main(void)
 	configura_ledin(); 
 	
 	sei();
-	char msg[] = " Atividade 8 – Interrupcoes temporizadas tratam concorrencia entre tarefas! \n";
+	char msg[] = " Atividade 8 â€“ Interrupcoes temporizadas tratam concorrencia entre tarefas! \n";
 
     while (1) 
     {
